4a4db0fe537e3b30829c2d495b67ae7d6cf7efa0
parfait.ntaganira <parntaga@gmail.com>
2026-06-10 14:49:17 -0400

    Initial Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,9 +8,19 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } } # Create a dictionary with the text to be analyzed 
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # Set the headers required for the API request 
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers 
-    return (response.text)
+    formatted_response = json.loads(response.text) # Return the response text from the API
     
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
     
+    for prediction in formatted_response['emotionPredictions']:
+        emotions = prediction['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
     
-    
-    
+    return {'anger':anger, 'disgust':disgust, 'fear':fear, 'joy':joy, 'sadness':sadness, 'dominant_emotion':dominant_emotion }
+
+
+
